45fc2cbd57d1b7557bf8c8757860f6630572ebdf
Maranatha A. Ilesanmi <maranatha.ilesanmi@gmail.com>
2019-03-19 23:46:36 +0100

    Add name to logo

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -35,6 +35,7 @@ class Navbar extends Component {
                             <circle cx="12" cy="12" r="10"/>
                             <path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/>
                         </svg>
+                        Newsglob
                     </Link>
                     <Link className="py-2 d-none d-md-inline-block" to="/sources">Sources</Link>
                     <Link className="py-2 d-none d-md-inline-block" to="/top-headlines">Top headlines</Link>
